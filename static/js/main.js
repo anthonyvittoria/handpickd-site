eadade1c453d0839078132fa0dba9657ed876ed7
Anthony Vittoria <a.vittoria0304@gmail.com>
2021-01-07 01:22:20 -0800

    remove testing console log

--- static/js/main.js
+++ static/js/main.js
@@ -86,10 +86,7 @@ $('#btnPicker').click(function() {
 
 $('#btnImage').click(function() {
     let ede = document.getElementById('eyedropperEnabled');
-    if (window.getComputedStyle(ede).display === "none") {
-        console.log("YO");
-        return; 
-    }
+    if (window.getComputedStyle(ede).display === "none") { return; }
     $('#logo1').hide();
     $('#logo2').show();
     $('#stageOne').fadeOut('fast');
